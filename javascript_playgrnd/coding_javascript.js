78e962ffe52acb737a903f6777439c51e37b19ec
arjun <arjunporwal10@gmail.com>
2018-07-06 14:27:44 +0530

    a simple game

--- javascript_playgrnd/coding_javascript.js
+++ javascript_playgrnd/coding_javascript.js
@@ -1,10 +1,21 @@
+
 window.onload=function(){
-    document.getElementById('content').addEventListener('click',respond);
-
+    
+   rnum=Math.floor((Math.random() * 10) + 1);
+   console.log(rnum);
+   document.getElementById('btnchk').addEventListener("click",checkguess);
 }
-
-function respond(e)
+var rnum;
+function checkguess(e)
 {
-   
-   console.log("you clicked");
+   var userguess = document.getElementById("guess").value;
+   console.log(userguess);
+   if(userguess == rnum)
+   {
+    document.getElementById('result').innerHTML="<h1>hello</h1>";
+   }
+   else
+   {
+    document.getElementById('result').innerHTML="<h1>wrrongs</h1>";
+   }
 }
