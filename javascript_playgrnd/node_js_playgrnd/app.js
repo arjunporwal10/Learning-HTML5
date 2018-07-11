3e2352f8b6f4e87e9c819d8dbc9cae0c5d018374
arjun <arjunporwal10@gmail.com>
2018-07-11 15:37:52 +0530

    pipes

--- javascript_playgrnd/node_js_playgrnd/app.js
+++ javascript_playgrnd/node_js_playgrnd/app.js
@@ -6,8 +6,4 @@ var myReadStream = fs.createReadStream(__dirname + '/readMe.txt','utf8');
 
 var myWriteStream = fs.createWriteStream(__dirname + '/writeMe.txt');
 
-myReadStream.on('data',function(chunk){
-    console.log('new chunk received');
-    myWriteStream.write(chunk);
-})
-
+myReadStream.pipe(myWriteStream);
