45ad13fa963c34c4d7e721bd938d0bddc270b1b4
arjun <arjunporwal10@gmail.com>
2018-07-11 14:14:59 +0530

    Events emitter

--- javascript_playgrnd/node_js_playgrnd/app.js
+++ javascript_playgrnd/node_js_playgrnd/app.js
@@ -1,4 +1,28 @@
-var stuff = require('./stuff');
-console.log(stuff.counter(['arj', 'dhjds','dkj']));
-console.log(stuff.adder(5,6));
-console.log(stuff.adder(stuff.pi,5));
+var events = require('events');
+var util = require('util');
+// var myEmitter = new events.EventEmitter();
+
+// myEmitter.on('someEvent', function(msg){
+//     console.log(msg);
+// });
+
+// myEmitter.emit('someEvent','the event was emitted');
+var Person = function(name){
+    this.name = name;
+};
+
+util.inherits(Person,events.EventEmitter);
+
+var James = new Person('James');
+var Jaden = new Person('Jaden');
+var John = new Person('John');
+var people = [James,Jaden,John];
+
+people.forEach(function(Person){
+    Person.on('speak',function(msg){
+        console.log(Person.name + ' said: ' + msg);
+    });
+});
+
+James.emit('speak','hey dude');
+John.emit('speak','yo');
